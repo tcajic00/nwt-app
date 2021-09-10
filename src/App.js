2e5c1d552e0c68fad611188511eaff070aadc584
Tomislav Cajic <tcajic00@fesb.hr>
2021-09-10 15:34:53 +0200

    Added titles and their styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Car repair queue</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add vehicle</h2>
+        </div>
+        <div className="flex-large" id="title-2">
+          <h2>Vehicle queue</h2>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
